a5d20927f0b9546a2f7406c80557bf7de0987852
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-13 11:01:25 -0300

    chore(backend): add basic Sequelize config

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'DIALECT_HERE',
+  username: 'DB_USERNAME_HERE',
+  password: 'DB_PASSWORD_HERE',
+  database: 'DB_NAME_HERE',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
